9d23f163c8f549cc3d5baaad0f66e192d7c4217b
U Roshan Kumar Patro <129484442+Roshan-P9@users.noreply.github.com>
2023-10-06 18:05:41 +0530

    Create Dannysdiner_schema.sql

---
+++ Case Study #1 - Danny's Diner/Dannysdiner_schema.sql
@@ -0,0 +1,17 @@
+TABLE sales {
+  "customer_id" VARCHAR(1)
+  "order_date" DATE
+  "product_id" INTEGER
+}
+
+TABLE menu {
+  "product_id" INTEGER
+  "product_name" VARCHAR(5)
+  "price" INTEGER
+}
+
+TABLE members {
+  "customer_id" VARCHAR(1)
+  "join_date" TIMESTAMP
+}
+
